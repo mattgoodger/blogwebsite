64e13d734be723155a24c6a246b0b35a1d80103e
jainarihant <arihant.jainr@atos.net>
2018-11-30 18:17:26 +0530

    some changes to fix the errors

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../config.service';
 import { PagerService } from '../pager.service';
-import { Post } from './post';
+// import { Post } from './post';
 
 
 @Component({
@@ -16,32 +16,32 @@ export class BlogComponent implements OnInit {
   pages: any[];
   pageSize = 3;
   pager: any = {};
-  posts: Post[];
+  // posts: Post[];
 
   constructor(private config: ConfigService, private pagerService: PagerService) { }
 
   ngOnInit() {
     this.blog = this.getBlog();
-    this.getPosts()
+    // this.getPosts()
     this.allItems = this.blog.posts;
     this.setPage(1);
-    }
+  }
 
-    getPosts(){
-      this.config.getPosts().subscribe(
-        posts => {
-          this.posts = posts;
-          this.allItems = this.posts;
-          this.setPage(1);
-        }
-      )
-    }
+  // getPosts() {
+  //   this.config.getPosts().subscribe(
+  //     posts => {
+  //       this.posts = posts;
+  //       this.allItems = this.posts;
+  //       this.setPage(1);
+  //     }
+  //   )
+  // }
 
   getBlog() {
     return this.config.getConfig().blog;
   }
 
-  setPage(pageNumber: number){
+  setPage(pageNumber: number) {
     this.pager = this.pagerService.getPager(this.allItems.length, pageNumber, this.pageSize);
 
     this.pages = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
